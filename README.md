c64df00257dab8dcace52bdcc12b89cdf7253ab9
itsmhmoon <mhmoon.info@gmail.com>
2022-01-29 02:05:00 +0600

    Basic Upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Alphabets-Flutter
+# English Alphabets in colorful UI.
+
+## UI:
+<p align="left"> <img src="assets/UI/UI01.png"> </p>
+
+## More updates will be available soon!
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'box.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'English Alphabets',
+      home: Box(),
+    );
+  }
+}
